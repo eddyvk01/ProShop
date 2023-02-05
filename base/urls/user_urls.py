b5285bdb6c377dcb0eb277ece64e18f7455cf385
vineet <vineet123@gmail.com>
2023-02-05 17:15:46 +0530

    Intial Commit

---
+++ base/urls/user_urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from base.views import user_views as views
+
+
+urlpatterns =[
+    # path('',views.getRoutes,name="routes"),
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('register/', views.registerUser,name ='register'),
+    path('profile/',views.getUserProfile,name="user-profile"),
+    path('',views.getUsers,name="user"),
+    path('profile/update/',views.updateUserProfile,name="user-profile-update"),   
+    path('<str:pk>/', views.getUserById, name='user'),
+    path('update/<str:pk>/', views.updateUser, name='user-update'), 
+    path('delete/<str:pk>/',views.deleteUser,name="user-delete"),
+]
