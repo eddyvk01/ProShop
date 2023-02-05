cb1f13745da5f718392f4ded6bd602dd2ce0ab8d
vineet <vineet123@gmail.com>
2023-02-05 18:42:11 +0530

    adding build file

---
+++ frontend/build/precache-manifest.c407200de1c38226f51fa840de8219e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea61acc5475e30494721",
+    "url": "/static/css/main.c93bcd02.chunk.css"
+  },
+  {
+    "revision": "ea61acc5475e30494721",
+    "url": "/static/js/main.ea61acc5.chunk.js"
+  },
+  {
+    "revision": "99eb1d9155ebc235c3d1",
+    "url": "/static/js/1.99eb1d91.chunk.js"
+  },
+  {
+    "revision": "f143f5e706cd81e1b5c2",
+    "url": "/static/js/2.f143f5e7.chunk.js"
+  },
+  {
+    "revision": "7fc94779e0a8ee2d3b56",
+    "url": "/static/js/runtime~main.7fc94779.js"
+  },
+  {
+    "revision": "8c3a3952732a53599b7b36c5f90f6ed4",
+    "url": "/index.html"
+  }
+];
